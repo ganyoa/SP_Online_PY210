4e360a6d3cd447bf18fa72afad57c43d34c69c84
Anthony Ganyo <ganyoa@gmail.com>
2020-03-11 22:08:53 +0300

    update to lesson08 exercise

--- students/ganyoa/lesson08/circle_class_exercise.py
+++ students/ganyoa/lesson08/circle_class_exercise.py
@@ -1,9 +1,85 @@
 #!/usr/bin/env python3
 
-class Circle:
+from math import pi
+
+
+class Circle(object):
 
     def __init__(self, radius):
         self.radius = radius
 
+    def __repr__(self): #step 6
+        return 'Circle({})'.format(self.radius)
+
+    def __str__(self): #step 6
+        return 'Circle with radius: {}'.format(self.radius)
+
+    @property
+    def radius(self):
+        return self._radius # _x is an internal value
+
+    @radius.setter # needed to see radius attribute
+    def radius(self, rad):
+        if rad < 0:
+            raise ValueError("radius can't be less than zero")
+        self._radius = rad
+
+#step 2, 3
+    @property
     def diameter(self):
-        return 2 * self.radius
+        return 2 * self._radius
+
+    @diameter.setter
+    def diameter(self, rad):
+        self.radius = rad / 2
+
+#step 4
+    @property
+    def area(self):
+        return pi * self._radius**2
+
+    @area.setter
+    def area(self, area):
+        raise AttributeError('Cannot set area')
+
+#step 5
+    @classmethod
+    def from_diameter(cls , dia):
+        return cls(dia / 2)
+
+#step 7
+    def __add__(self, other):
+        return Circle(self.radius + other.radius)
+
+    def __mul__(self, other):
+        return Circle(self.radius * other)
+
+#step 8
+    def __gt__(self, other):
+        return self.radius > other.radius
+
+    def __lt__(self, other):
+        return self.radius < other.radius
+
+    def __eq__(self, other):
+        return self.radius == other.radius
+
+    def sort_key(self):
+        return self.radius
+
+#step 9
+class Sphere(Circle):
+
+    def __repr__(self): #step 6
+        return 'Sphere({})'.format(self.radius)
+
+    def __str__(self): #step 6
+        return 'Sphere with radius: {}'.format(self.radius)
+
+    @property
+    def volume(self):
+        return ((4/3) * pi * self.radius**3)
+
+    @property
+    def area(self):
+        return (4 * pi * self.radius**2)
--- students/ganyoa/lesson08/test_circle_class_exercise.py
+++ students/ganyoa/lesson08/test_circle_class_exercise.py
@@ -1,9 +1,92 @@
 from circle_class_exercise import *
 
+#step 1
 def test_radius():
     c = Circle(4)
     assert c.radius == 4
 
+#step 2
 def test_diameter():
     c = Circle(4)
-    assert c.diameter() == 8
+    assert c.diameter == 8
+
+#step 3
+def test_diameter_to_radius():
+    c = Circle(4)
+    c.diameter = 20
+    assert c.radius == 10
+
+#step 4
+def test_area():
+    c = Circle(2)
+    assert c.area == 12.566370614359172
+
+#step 5
+def test_from_diameter():
+     c = Circle.from_diameter(8)
+     assert c.diameter == 8
+     assert c.radius == 4
+     assert c.area == 50.26548245743669
+
+#step 6
+def test_print():
+    c = Circle(32)
+    assert c.__str__() == 'Circle with radius: 32'
+    assert c.__repr__() == 'Circle(32)'
+
+#step 7
+def test_add():
+    c1 = Circle(2)
+    c2 = Circle(4)
+    assert repr(Circle(6)) == repr(c1 + c2)
+
+def test_mul():
+    c2 = Circle(4)
+    assert repr(c2 * 3) == repr(Circle(12))
+
+#step 8
+def test_comparisons():
+    c1 = Circle(5)
+    c2 = Circle(7)
+    c3 = Circle(7)
+    assert c2 > c1
+    assert c2 == c3
+
+def test_sort():
+    a_list = [Circle(6), Circle(7), Circle(8), Circle(4), Circle(0), Circle(2), Circle(3), Circle(5), Circle(9), Circle(1)]
+    b_list = a_list[:]
+    a_list.sort()
+    assert a_list == [Circle(0), Circle(1), Circle(2), Circle(3), Circle(4), Circle(5), Circle(6), Circle(7), Circle(8), Circle(9)]
+    assert sorted(b_list, key=Circle.sort_key) == [Circle(0), Circle(1), Circle(2), Circle(3), Circle(4), Circle(5), Circle(6), Circle(7), Circle(8), Circle(9)]
+
+#step 9
+def test_sphere_print():
+    s = Sphere(32)
+    assert s.__str__() == 'Sphere with radius: 32'
+    assert s.__repr__() == 'Sphere(32)'
+
+def test_sphere_rad():
+    s = Sphere(5)
+    assert s.radius == 5
+
+def test_sphere_vol():
+    s = Sphere(5)
+    assert s.volume == 523.5987755982989
+
+def test_sphere_area():
+    s = Sphere(5)
+    c = Circle(2)
+    assert s.area == 314.1592653589793
+    assert c.area == 12.566370614359172
+
+def test_sphere_comparisons():
+    s1 = Sphere(5)
+    s2 = Sphere(7)
+    s3 = Sphere(7)
+    assert s2 > s1
+    assert s2 == s3
+
+def test_sphere_diameter_to_radius():
+    s = Sphere(4)
+    s.diameter = 20
+    assert s.radius == 10
